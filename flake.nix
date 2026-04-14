64ca2f93c4ab933e4d99cf01c0fc2ca12de3f0cf
Noa Aarts <noa@voorwaarts.nl>
2026-04-14 20:45:07 +0200

    add tensorcircuit

--- flake.nix
+++ flake.nix
@@ -25,6 +25,7 @@
             pkgs = import nixpkgs { inherit system; };
           }
         );
+
     in
     {
       devShells = forAllSystems (
@@ -34,6 +35,99 @@
           system,
           ...
         }:
+        let
+          tensornetwork-ng = pkgs.python3.pkgs.buildPythonPackage (finalAttrs: {
+            pname = "tensornetwork-ng";
+            version = "0.5.1";
+            pyproject = true;
+
+            src = pkgs.fetchPypi {
+              pname = "tensornetwork_ng";
+              inherit (finalAttrs) version;
+              hash = "sha256-sSMHOWnDpUQLw2T3oMHkKLGdO3wMe0x+aqLpNr19uvM=";
+            };
+
+            build-system = [
+              pkgs.python3.pkgs.setuptools
+            ];
+
+            dependencies = with pkgs.python3.pkgs; [
+              graphviz
+              h5py
+              numpy
+              opt-einsum
+              scipy
+            ];
+
+            pythonImportsCheck = [
+              "tensornetwork"
+            ];
+
+            passthru.updateScript = pkgs.nix-update-script { };
+
+            meta = {
+              description = "A high level tensor network API for accelerated tensor network calculations";
+              homepage = "https://pypi.org/project/tensornetwork-ng";
+              license = pkgs.lib.licenses.asl20;
+            };
+          });
+
+          tensorcircuit = pkgs.python3.pkgs.buildPythonPackage (finalAttrs: {
+            pname = "tensorcircuit-ng";
+            version = "1.6.0";
+            pyproject = true;
+
+            src = pkgs.fetchPypi {
+              pname = "tensorcircuit_ng";
+              inherit (finalAttrs) version;
+              hash = "sha256-G0KK05vUmm65b65XAP7sgwIGX0EpPqXG5OPW2I1L3w0=";
+            };
+
+            build-system = [
+              pkgs.python3.pkgs.setuptools
+            ];
+
+            dependencies = with pkgs.python3.pkgs; [
+              networkx
+              numpy
+              scipy
+              tensornetwork-ng
+            ];
+
+            optional-dependencies = with pkgs.python3.pkgs; {
+              cloud = [
+                mthree
+                qiskit
+              ];
+              jax = [
+                jax
+                jaxlib
+              ];
+              qiskit = [
+                qiskit
+                symengine
+                sympy
+              ];
+              tensorflow = [
+                tensorflow
+              ];
+              torch = [
+                torch
+              ];
+            };
+
+            pythonImportsCheck = [
+              "tensorcircuit"
+            ];
+
+            meta = {
+              description = "High performance unified quantum computing framework for the NISQ era";
+              homepage = "https://pypi.org/project/tensorcircuit-ng";
+              license = pkgs.lib.licenses.asl20;
+              mainProgram = "tensorcircuit-ng";
+            };
+          });
+        in
         {
           default = pkgs.mkShellNoCC {
             packages = [
@@ -46,6 +140,9 @@
                 ppkgs.matplotlib
                 ppkgs.pytest
                 ppkgs.pytest-xdist
+                ppkgs.jax
+                ppkgs.tensorflow
+                tensorcircuit
               ]))
             ];
           };
